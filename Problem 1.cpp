5abdf3b6b2acc6fbf800515cca396d7ae062953b
Kobe Acob <47447956+kobewan23@users.noreply.github.com>
2019-05-23 23:51:28 +0800

    Add files via upload

---
+++ Problem 1.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[40];
+    int size, x, y, z;
+    cout << "Enter array size: ";
+    cin >> size;
+    
+    cout << "Enter elements: " << endl;
+    for (x = 0; x < size; x++)
+    {
+        cin >> arr[x];
+    }
+    
+    cout << "List of elements: " << endl;
+    
+    for (x = 0; x < size; x++)
+    {
+        cout << arr[x]<< ", ";
+    }
+    
+    for (x = 0; x < size; x++)
+    {
+        for (y = x + 1; y< size; y++)
+        {
+            if (arr[x] > arr[y])
+            {
+                z = arr[x];
+                arr[x] = arr[y];
+                arr[y] = z;
+            }
+        }
+    }
+    
+    cout << "\nAfter using selection sort..." << endl;
+    cout << "data:" << endl;
+    
+    for (x = 0; x < size; x++)
+    {
+        cout << arr[x] << " ";
+    }
+    _getch();
+    return 0;
+}
